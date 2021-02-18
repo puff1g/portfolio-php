8d5cd4bd8940beef2ea4c786c4b397882dd2ae84
DESKTOP-2VFB9CV\tke20 <tke2000wow@gmail.com>
2021-02-18 13:04:06 +0100

    Made contact Page a bit better

--- wp-content/themes/layback-child/partials/blocks/ContactMain/block.php
+++ wp-content/themes/layback-child/partials/blocks/ContactMain/block.php
@@ -1,18 +1,18 @@
 <?php
 
-	add_action('acf/init', 'lb_register_Heroo');
-	function lb_register_Heroo()
+	add_action('acf/init', 'lb_register_ContactPage');
+	function lb_register_ContactPage()
 	{
 
 	    // check function exists.
 	    if( function_exists('acf_register_block_type') )
 	    {
 
-	    	$title 					= __('Hero', 'layback');
+	    	$title 					= __('Contact', 'layback');
 	    	$description 			= __('DESCRIPTION', 'layback');
 	    	$tags 					=	array('DEMO');
 	    	$align 					= array('wide', 'full');
-	    	$render 				= 'lb_register_Heroorender';
+	    	$render 				= 'lb_register_ContactPagerender';
 
 	        // register a testimonial block.
 	        acf_register_block_type(array(
@@ -30,11 +30,11 @@
 				'render_callback'   => $render,
 				'enqueue_style' 	=> get_stylesheet_directory_uri() . '/partials/blocks/' . basename(__DIR__) . '/style.css',
 				'enqueue_script' 	=> get_stylesheet_directory_uri() . '/partials/blocks/' . basename(__DIR__) . '/script.js',
-	));
-	}
+	        ));
+	    }
 	}
 
-	function lb_register_Heroorender( $block, $content = '', $is_preview = false, $post_id = 0 )
+	function lb_register_ContactPagerender( $block, $content = '', $is_preview = false, $post_id = 0 )
 	{
 
 		/* Add all variables in the top
@@ -51,30 +51,8 @@
 
 	?>
 	
-	<div id="<?php echo $block_id; ?>" class="<?php if( !empty($block_align) ) { echo 'align-' . $block_align; } ?> block-<?php echo $block_name; ?>">
-		<div class="bannerimage">
-			<div class="imaget">
-				<img src="<?php echo wp_get_attachment_url(53); ?>">
-				
-				<div class="Blureffect"></div>
-				<div class="Maintxt">
-					<div class="smalltxt">
-						<h1>Developer in learning</h1>
-					</div>
-					<div class="biggrtxt">
-						<h1>Creating leg-sweeping <br />
-							apps & websites.</h1>
-					</div>
-					<div class="buttonsban">
-						<a href="<?php echo get_post_type_archive_link( "project" ); ?>" ><button class="button1"> View Projects</button></a>
-						<a href="<?php echo get_post_type_archive_link( "" ); ?>"><button class="button2">Contact Me</button></a> <!-- //TODO Setup Permalinks to work so this can work -->
-					</div>
-					
-				</div>
-			</div>
-			<div>
-			</div>
+		<div class="space">
+			<h1>Feel free to get in contact with me!</h1>
 		</div>
-	</div>
-    
+	
     <?php }
---
+++ wp-content/themes/layback-child/partials/template-ContactMe.php
@@ -0,0 +1,9 @@
+<?php 
+/*
+        *
+        * Template name: Contact me
+        *
+    */
+    ?>
+<?php get_header(); ?>
+<?php get_footer(); ?>
